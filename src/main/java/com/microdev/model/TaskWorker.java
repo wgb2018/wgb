5f683061118d9337f8cce36ffeb8be1b9cf64516
zhanglin <525975661@qq.com>
2018-07-09 17:07:31 +0800

    2018070917

--- src/main/java/com/microdev/model/TaskWorker.java
+++ src/main/java/com/microdev/model/TaskWorker.java
@@ -133,10 +133,7 @@ public class TaskWorker extends BaseEntity {
     @JsonFormat(pattern="HH:mm")
     private OffsetTime dayEndTime;
     private String hotelTaskId;
-    /**
-     * 是否已查看0未读1未完成已读3已完成已读
-     */
-    private Integer checkSign = 0;
+
     /**
     * 是否验证
     * */
